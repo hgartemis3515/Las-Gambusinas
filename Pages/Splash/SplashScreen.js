c5d542a660e3c4d9f578917d7bc4a9bfc4948dad
hgartemis3515 <hgartemis3515@gmail.com>
2026-09-13 18:40:03 -0500

    1.9.1v

--- Pages/Splash/SplashScreen.js
+++ Pages/Splash/SplashScreen.js
@@ -19,7 +19,7 @@ import { colors } from '../../constants/colors';
 import { checkAndApplyOtaUpdate, getOtaRuntimeInfo } from '../../services/otaUpdates';
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
-const APP_VERSION = Constants.expoConfig?.version || '1.9v';
+const APP_VERSION = Constants.expoConfig?.version || '1.9.1v';
 
 // Partículas flotantes de fondo
 const FloatingParticle = ({ delay, startY }) => {
