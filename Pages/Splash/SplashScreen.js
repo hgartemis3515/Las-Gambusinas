230c2fbe039301e163f5c6f4d48a8781b3340d48
hgartemis3515 <hgartemis3515@gmail.com>
2026-09-18 16:15:23 -0500

    App 2.0v

--- Pages/Splash/SplashScreen.js
+++ Pages/Splash/SplashScreen.js
@@ -16,7 +16,7 @@ import Constants from 'expo-constants';
 import { colors } from '../../constants/colors';
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
-const APP_VERSION = Constants.expoConfig?.version || '1.9.19v';
+const APP_VERSION = Constants.expoConfig?.version || '2.0v';
 
 // Partículas flotantes de fondo
 const FloatingParticle = ({ delay, startY }) => {
